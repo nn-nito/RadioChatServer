16999bb2b16d90152ff19b8b83747df6bb6884f7
nn-nito <retou.1007@gmail.com>
2020-03-21 00:15:14 +0000

    レイアウト作成：おすすめの番組のレイアウト作成

--- resources/views/base/common.blade.php
+++ resources/views/base/common.blade.php
@@ -51,12 +51,12 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">リアルタイム掲示板</a>
                 </li>
-{{--                <li class="nav-item">--}}
-{{--                    <a class="nav-link" href="#">特集</a>--}}
-{{--                </li>--}}
-{{--                <li class="nav-item">--}}
-{{--                    <a class="nav-link" href="#">サイトについて</a>--}}
-{{--                </li>--}}
+                {{--                <li class="nav-item">--}}
+                {{--                    <a class="nav-link" href="#">特集</a>--}}
+                {{--                </li>--}}
+                {{--                <li class="nav-item">--}}
+                {{--                    <a class="nav-link" href="#">サイトについて</a>--}}
+                {{--                </li>--}}
             </ul>
         </div>
     </nav>
@@ -69,7 +69,7 @@
                 <div class="d-flex flex-column">
                     <p style="text-align: center; font-weight: bold; font-size: 120%">声優ラジオ検索</p>
                     <form class="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2" id="search_box" type="text" placeholder="声優またはラジオの名前入れてね">
+                        <input class="form-control mr-sm-2" id="search_box" type="text" placeholder="声優さんの名前フルネームで入れてね">
                         <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">検索</button>
                     </form>
                     <div class="d-flex justify-content-center my-2">
@@ -88,11 +88,99 @@
                 <h2 class="hs__headline"><i class="fas fas fa-burn faa-horizontal animated my-base-color"></i> おすすめの番組</h2>
             </div>
             <ul class="slider">
-                <li><a href="#"><img src="https://picsum.photos/id/112/300/300" alt="image01"></a></li>
-                <li><a href="#"><img src="https://picsum.photos/id/122/300/300" alt="image02"></a></li>
-                <li><a href="#"><img src="https://picsum.photos/id/132/300/300" alt="image03"></a></li>
-                <li><a href="#"><img src="https://picsum.photos/id/142/300/300" alt="image03"></a></li>
-                <li><a href="#"><img src="https://picsum.photos/id/152/300/300" alt="image03"></a></li>
+                {{-- 本渡上陸作戦 --}}
+                <li>
+                    <div class="card mb-3">
+                        <img style="width:auto; height:auto; max-width:100%; max-height:100%;display: block; object-fit: cover" src="https://pbs.twimg.com/profile_banners/715773402905182209/1554182718/1500x500" alt="image01">
+                        <div class="card-body">
+                            <p class="card-text" style="color: #EB6864">
+                                <i class="fas fa-tv"></i> 本渡上陸作戦
+                            </p>
+                            <p class="card-text" style="color: #EB6864">
+                                <i class="fas fa-info-circle"></i> 文化放送『超！A&G＋』で毎週月曜 21:30~22:00に放送中
+                            </p>
+                            <p class="card-text">
+                                <a href="https://twitter.com/hjsakusen?lang=ja">
+                                    <i class="fab fa-twitter"></i><span>  公式Twitterアカウント</span>
+                                </a>
+                            </p>
+                            <p class="card-text" style="text-align: center">
+                                <i class="fas fa-user-circle"></i>
+                                <a href="https://twitter.com/hjsakusen?lang=ja">#本渡楓</a>
+                                <a href="https://twitter.com/hjsakusen?lang=ja">#天津向</a>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                {{-- 小原好美のココロおきなく --}}
+                <li>
+                    <div class="card mb-3">
+                        <img style="width:auto; height:auto; max-width:100%; max-height:100%;display: block; object-fit: cover" src="https://pbs.twimg.com/profile_banners/1123090571604586496/1556953470/1080x360" alt="image01">
+                        <div class="card-body">
+                            <p class="card-text" style="color: #EB6864">
+                                <i class="fas fa-tv"></i> 小原好美のココロおきなく
+                            </p>
+                            <p class="card-text" style="color: #EB6864">
+                                <i class="fas fa-info-circle"></i> 文化放送「超!A&G＋」にて【毎週日曜20:00～20:30】に放送！
+                            </p>
+                            <p class="card-text">
+                                <a href="https://twitter.com/kokoradi_joqr">
+                                    <i class="fab fa-twitter"></i><span>  公式Twitterアカウント</span>
+                                </a>
+                            </p>
+                            <p class="card-text" style="text-align: center">
+                                <i class="fas fa-user-circle"></i>
+                                <a href="https://twitter.com/hjsakusen?lang=ja">#小原好美</a>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                {{-- 田中美海のかもん！みなはうす♫ --}}
+                <li>
+                    <div class="card mb-3">
+                        <img style="width:auto; height:auto; max-width:100%; max-height:100%;display: block; object-fit: cover" src="https://pbs.twimg.com/profile_banners/1209789904109965314/1584464403/1080x360" alt="image01">
+                        <div class="card-body">
+                            <p class="card-text" style="color: #EB6864">
+                                <i class="fas fa-tv"></i> 田中美海のかもん！みなはうす♫
+                            </p>
+                            <p class="card-text" style="color: #EB6864">
+                                <i class="fas fa-info-circle"></i> 文化放送超A&G +で毎週火曜25時30分から放送中の番組（はうす)
+                            </p>
+                            <p class="card-text">
+                                <a href="https://twitter.com/minahouse_joqr">
+                                    <i class="fab fa-twitter"></i><span>  公式Twitterアカウント</span>
+                                </a>
+                            </p>
+                            <p class="card-text" style="text-align: center">
+                                <i class="fas fa-user-circle"></i>
+                                <a href="https://twitter.com/hjsakusen?lang=ja">#田中美海</a>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                {{-- 井口裕香のむ～～～ん ⊂(　＾ω＾)⊃ --}}
+                <li>
+                    <div class="card mb-3">
+                        <img style="width:auto; height:auto; max-width:100%; max-height:100%;display: block; object-fit: cover" src="https://pbs.twimg.com/profile_banners/3191805905/1560784128/1080x360" alt="image01">
+                        <div class="card-body">
+                            <p class="card-text" style="color: #EB6864">
+                                <i class="fas fa-tv"></i> 井口裕香のむ～～～ん
+                            </p>
+                            <p class="card-text" style="color: #EB6864">
+                                <i class="fas fa-info-circle"></i> 文化放送 超！Ａ＆Ｇ＋で毎週月曜22:00～生放送でお届け
+                            </p>
+                            <p class="card-text">
+                                <a href="https://twitter.com/muuun_ag">
+                                    <i class="fab fa-twitter"></i><span>  公式Twitterアカウント</span>
+                                </a>
+                            </p>
+                            <p class="card-text" style="text-align: center">
+                                <i class="fas fa-user-circle"></i>
+                                <a href="https://twitter.com/hjsakusen?lang=ja">#井口裕香</a>
+                            </p>
+                        </div>
+                    </div>
+                </li>
             </ul>
         </div>
     </div>
@@ -152,14 +240,14 @@
 </footer>
 
 <script>
-    // カルーセル
+	// カルーセル
 	$('.slider').slick({
 		centerMode: true,
 		centerPadding: '80px',
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		autoplay: true,
-		autoplaySpeed: 2000,
+		autoplaySpeed: 6000,
 		dots: true,
 		responsive: [
 			{
@@ -183,19 +271,19 @@
 	});
 
 	// スムーズスクロール
-	$(function(){
+	$(function () {
 		// #で始まるアンカーをクリックした場合に処理
-		$('a[href^="#"]').click(function() {
+		$('a[href^="#"]').click(function () {
 			// スクロールの速度
 			var speed = 500; // ミリ秒
 			// アンカーの値取得
-			var href= $(this).attr("href");
+			var href = $(this).attr("href");
 			// 移動先を取得
 			var target = $(href == "#" || href == "" ? 'html' : href);
 			// 移動先を数値で取得
 			var position = target.offset().top;
 			// スムーススクロール
-			$('body,html').animate({scrollTop:position}, speed, 'swing');
+			$('body,html').animate({scrollTop: position}, speed, 'swing');
 			return false;
 		});
 	});
